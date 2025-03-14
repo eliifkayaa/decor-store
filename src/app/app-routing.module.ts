03b0c509b564aac76991370c9dba31511a824a19
Elif Gokce <elifkayagokce@outlook.com>
2025-03-14 17:04:30 +0300

    routing-module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', loadChildren: () => import('./components/demo-decor-store/modules/home.module').then(m => m.HomeModule)},
+  {path: 'about', loadChildren: () => import('./components/demo-decor-store-about/modules/about.module').then(m => m.AboutModule)},
+  {path: 'account', loadChildren: () => import('./components/demo-decor-store-account/modules/account.module').then(m => m.AccountModule)},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
