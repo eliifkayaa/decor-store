6666d3c0d40f32d6575e094f0b75f1b45c6a9935
Elif Gokce <elifkayagokce@outlook.com>
2025-03-15 11:17:00 +0300

    routing structure created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,19 @@ const routes: Routes = [
   {path: '', loadChildren: () => import('./components/demo-decor-store/modules/home.module').then(m => m.HomeModule)},
   {path: 'about', loadChildren: () => import('./components/demo-decor-store-about/modules/about.module').then(m => m.AboutModule)},
   {path: 'account', loadChildren: () => import('./components/demo-decor-store-account/modules/account.module').then(m => m.AccountModule)},
+  {path: 'blog', loadChildren: () => import('./components/demo-decor-store-blog/modules/blog.module').then(m => m.BlogModule)},
+  {path: 'blog-single', loadChildren: () => import('./components/demo-decor-store-blog-single-classic/modules/blog-single.module').then(m => m.BlogSingleModule)},
+  {path: 'cart', loadChildren: () => import('./components/demo-decor-store-blog/modules/blog.module').then(m => m.BlogModule)},
+  {path: 'checkout', loadChildren: () => import('./components/demo-decor-store-checkout/modules/checkout.module').then(m => m.CheckoutModule)},
+  {path: 'collections', loadChildren: () => import('./components/demo-decor-store-collections/modules/collections.module').then(m => m.CollectionsModule)},
+  {path: 'contact', loadChildren: () => import('./components/demo-decor-store-contact/modules/contact.module').then(m => m.ContactModule)},
+  {path: 'faq', loadChildren: () => import('./components/demo-decor-store-faq/modules/faq.module').then(m => m.FaqModule)},
+  {path: 'no-sidebar', loadChildren: () => import('./components/demo-decor-store-no-sidebar/modules/no-sidebar.module').then(m => m.NoSidebarModule)},
+  {path: 'right-sidebar', loadChildren: () => import('./components/demo-decor-store-right-sidebar/modules/right-sidebar.module').then(m => m.RightSidebarModule)},
+  {path: 'product', loadChildren: () => import('./components/demo-decor-store-single-product/modules/single-product.module').then(m => m.SingleProductModule)},
+  {path: 'wishlist', loadChildren: () => import('./components/demo-decor-store-wishlist/modules/wishlist.module').then(m => m.WishlistModule)},
+  {path: 'shop', loadChildren: () => import('./components/demo26-shop/modules/shop.module').then(m => m.ShopModule)},
+  {path: '**', redirectTo: ''}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,41 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
-import { DemoDecorStoreAboutComponent } from './components/demo-decor-store-about/demo-decor-store-about.component';
-import { DemoDecorStoreAccountComponent } from './components/demo-decor-store-account/demo-decor-store-account.component';
-import { DemoDecorStoreBlogSingleClassicComponent } from './components/demo-decor-store-blog-single-classic/demo-decor-store-blog-single-classic.component';
-import { DemoDecorStoreBlogComponent } from './components/demo-decor-store-blog/demo-decor-store-blog.component';
-import { DemoDecorStoreCartComponent } from './components/demo-decor-store-cart/demo-decor-store-cart.component';
-import { DemoDecorStoreCheckoutComponent } from './components/demo-decor-store-checkout/demo-decor-store-checkout.component';
-import { DemoDecorStoreCollectionsComponent } from './components/demo-decor-store-collections/demo-decor-store-collections.component';
-import { DemoDecorStoreContactComponent } from './components/demo-decor-store-contact/demo-decor-store-contact.component';
-import { DemoDecorStoreFaqComponent } from './components/demo-decor-store-faq/demo-decor-store-faq.component';
-import { DemoDecorStoreNoSidebarComponent } from './components/demo-decor-store-no-sidebar/demo-decor-store-no-sidebar.component';
-import { DemoDecorStoreRightSidebarComponent } from './components/demo-decor-store-right-sidebar/demo-decor-store-right-sidebar.component';
-import { DemoDecorStoreSingleProductComponent } from './components/demo-decor-store-single-product/demo-decor-store-single-product.component';
-import { DemoDecorStoreWishlistComponent } from './components/demo-decor-store-wishlist/demo-decor-store-wishlist.component';
-import { DemoDecorStoreComponent } from './components/demo-decor-store/demo-decor-store.component';
-import { Demo26ShopComponent } from './components/demo26-shop/demo26-shop.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DemoDecorStoreAboutComponent,
-    DemoDecorStoreAccountComponent,
-    DemoDecorStoreBlogSingleClassicComponent,
-    DemoDecorStoreBlogComponent,
-    DemoDecorStoreCartComponent,
-    DemoDecorStoreCheckoutComponent,
-    DemoDecorStoreCollectionsComponent,
-    DemoDecorStoreContactComponent,
-    DemoDecorStoreFaqComponent,
-    DemoDecorStoreNoSidebarComponent,
-    DemoDecorStoreRightSidebarComponent,
-    DemoDecorStoreSingleProductComponent,
-    DemoDecorStoreWishlistComponent,
-    DemoDecorStoreComponent,
-    Demo26ShopComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
