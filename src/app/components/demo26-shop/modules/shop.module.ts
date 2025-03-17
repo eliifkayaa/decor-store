686ed51ad546de3ed5cede6a4e3f7059e9e60276
Elif Gokce <elifkayagokce@outlook.com>
2025-03-17 15:02:00 +0300

    style resolved

--- src/app/components/demo26-shop/modules/shop.module.ts
+++ src/app/components/demo26-shop/modules/shop.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { Demo26ShopComponent } from '../demo26-shop.component';
 import { ShopRoutingModule } from './shop-routing.module';
+import { SharedModule } from '../../../module/shared/shared.module';
 
 
 @NgModule({
   declarations: [Demo26ShopComponent],
   imports: [
-    CommonModule,
-    ShopRoutingModule
+    SharedModule,
+    ShopRoutingModule,
   ]
 })
 export class ShopModule { }
